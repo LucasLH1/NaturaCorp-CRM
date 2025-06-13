8fcb931b8ca194541b0b507f930ca15cd61b7ca3
LucasLH1 <lahayelucas5@gmail.com>
2025-06-13 15:29:31 +0200

    Création des fichiers blade des vues

---
+++ resources/views/pharmacies/index.blade.php
@@ -0,0 +1,10 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800">Liste des pharmacies</h2>
+    </x-slot>
+
+    <div class="py-6 px-4">
+        <!-- Contenu ici -->
+        <p>Contenu à venir</p>
+    </div>
+</x-app-layout>
