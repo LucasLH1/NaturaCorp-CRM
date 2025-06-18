60c9d2d91ef00a62728e00fd12b1e3cda6e10156
LucasLH1 <lahayelucas5@gmail.com>
2025-06-18 21:32:17 +0200

    Commande Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            RoleSeeder::class,
-            DashboardDataSeeder::class,
+//            RoleSeeder::class,
+//            DashboardDataSeeder::class,
+            CommandeSeeder::class,
         ]);
     }
 
